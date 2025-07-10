a8b78367eb297b64906a5b7caff2421c3bfe6707
francuzzito <56544800+francuzzito@users.noreply.github.com>
2025-07-10 19:02:22 +0200

    constructor functions

--- learning_stage2/classes_and_objects.cpp
+++ learning_stage2/classes_and_objects.cpp
@@ -3,18 +3,27 @@
 
 class c_book {
 public:
-	std::string tittle;
+	std::string title;
 	std::string author;
 	int pages;
+
+	c_book() {
+		title = "no title";
+		author = "no author";
+		pages = 0;
+	}
+
+	c_book(std::string _title, std::string _author, int _pages) {
+		title = _title;
+		author = _author;
+		pages = _pages;
+	}
 };
 
 void callable_functions::classes_and_objects() {
-	c_book book1;
-	book1.tittle = "lol guide book";
-	book1.author = "greatest author alive";
-	book1.pages = 1337;
+	c_book book1("lol book", "greatest author alive", 1337);
 
-	std::cout << "book title: " << book1.tittle << "\n";
+	std::cout << "book title: " << book1.title << "\n";
 	std::cout << "book author: " << book1.author << "\n";
 	std::cout << "book pages: " << book1.pages << "\n";
 }
