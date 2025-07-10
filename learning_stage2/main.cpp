21abfdf04ff107cc6882f605f30349bb2969e5be
francuzzito <56544800+francuzzito@users.noreply.github.com>
2025-07-10 19:46:45 +0200

    fial version moving on to project 3 soon

--- learning_stage2/main.cpp
+++ learning_stage2/main.cpp
@@ -2,13 +2,14 @@
 
 int main() {
 	// going to re add while loop to be able to play multiple times and choose between games.
-	// callable_functions::guessing_game();
+	// leaving it like this, my previous source had it done but i dont bothere here :p
+	callable_functions::guessing_game();
 	// callable_functions::for_loops();
 	// callable_functions::twod_arrays_and_nested_loops();
 	// callable_functions::pointers();
 	// callable_functions::classes_and_objects();
 	// callable_functions::object_functions();
-	callable_functions::getters_and_setters();
+	// callable_functions::getters_and_setters();
 
 	return 0;
 }
